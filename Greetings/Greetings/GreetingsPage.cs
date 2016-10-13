8963bcb2c71ed01e3962ddb7e67bc8a2d9f0b8e6
David <david@fiveklines.com>
2016-10-13 10:35:32 -0700

    step 1

---
+++ Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Label label = new Label();
+            label.Text = "Greetings, Xamarin.Forms!";
+            this.Content = label;
+        }
+    }
+}
